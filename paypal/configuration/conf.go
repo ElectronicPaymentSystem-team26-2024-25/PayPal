1b82f22e2690f4b63245e10c774ab3fdd3cc3725
Наталија Поповић <natalijapopovic.rs@gmail.com>
2025-09-21 13:09:06 +0200

    fix: Fixed Heartbeat signal from PayPal

--- paypal/configuration/conf.go
+++ paypal/configuration/conf.go
@@ -2,6 +2,8 @@ package configuration
 
 import (
 	"fmt"
+	"log"
+	"time"
 
 	"github.com/ArthurHlt/go-eureka-client/eureka"
 )
@@ -12,16 +14,29 @@ func EurekaClientConfig() {
 	fmt.Println("Printing Client Details..")
 	fmt.Println(client)
 	instance := eureka.NewInstanceInfo(
-		"localhost",
+		"paypal-host",
 		"PAYPAL-SERVICE",
 		"127.0.0.1",
-		443,
+		8443,
 		30,
-		false,
+		true,
 	)
 
 	client.RegisterInstance("PAYPAL-SERVICE", instance)
-	client.SendHeartbeat(instance.App, instance.HostName)
+	go func() {
+		ticker := time.NewTicker(25 * time.Second) // send before lease expires
+		defer ticker.Stop()
+
+		for range ticker.C {
+			err := client.SendHeartbeat(instance.App, instance.HostName)
+			if err != nil {
+				log.Printf("Heartbeat failed: %v", err)
+			} else {
+				log.Printf("Heartbeat sent for %s (%s)", instance.App, instance.HostName)
+			}
+		}
+	}()
+
 	fmt.Println("Printing Instance Details...")
-	fmt.Println(client.GetInstance(instance.App, instance.HostName))
+	fmt.Println(client.GetInstance(instance.App, instance.InstanceID))
 }
